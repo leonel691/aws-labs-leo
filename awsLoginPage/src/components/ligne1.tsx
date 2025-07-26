acfc985ab102342cf966f3f809c8f3a549beb5a8
leonel691 <leonelngueaho@gmail.com>
2025-07-26 18:45:47 +0100

    visibility and not for input password

--- awsLoginPage/src/components/ligne1.tsx
+++ awsLoginPage/src/components/ligne1.tsx
@@ -1,6 +1,6 @@
 export default function LignePassword(){
     return(
-    <div className="grid grid-cols-4 gap-2 mt-3">
+    <div id="streng" className="grid grid-cols-4 gap-2 mt-3">
         <div className="rounded-xl h-2 w-full bg-[var(--bg-validation)]"></div>
         <div className="rounded-xl h-2 w-full bg-[var(--bg-validation)]"></div>
         <div className="rounded-xl h-2 w-full bg-[var(--bg-validation)]"></div>
@@ -8,3 +8,4 @@ export default function LignePassword(){
     </div>
     )
 }
+
