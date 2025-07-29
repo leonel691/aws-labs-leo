2cfebbe7b01337305deb29c70d91b287d55bc41b
leonel691 <leonelngueaho@gmail.com>
2025-07-29 13:21:22 +0100

    feat : responsive under 768px

--- awsLoginPage/src/components/ouAvec.tsx
+++ awsLoginPage/src/components/ouAvec.tsx
@@ -2,9 +2,9 @@
 export default function Ligne2(){
     return(
         <div className="items-center flex justify-center gap-9 mt-5 ">
-            <div className="bg-[var(--border-color)] w-[30%] h-[0.1rem] sm:w-[37%] sm:h-[0.18rem]"></div>
+            <div className="bg-[var(--border-color)] w-[30%] h-[0.1rem] sm:w-[37%] md:w-[39.5%] sm:h-[0.18rem]"></div>
             <span className="text-[var(--first-text-color)] font-inter text-[0.8rem] sm:text-[0.9rem]">OU AVEC</span>
-            <div className="bg-[var(--border-color)] w-[30%] h-[0.1rem] sm:w-[37%] sm:h-[0.18rem]"></div>
+            <div className="bg-[var(--border-color)] w-[30%] h-[0.1rem] sm:w-[37%] md:w-[39.5%] sm:h-[0.18rem]"></div>
         </div>
     )
 }
